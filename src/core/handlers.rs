bf1a9c36304c4c308066866d2e6d8d8bdfa6fd5f
noreplydev <contact.cristiansanchez@gmail.com>
2024-01-24 02:19:06 +0100

    [FT] Implemented print() core handler

--- src/core/handlers.rs
+++ src/core/handlers.rs
@@ -4,27 +4,35 @@ use crate::{
 };
 
 pub fn print(node: AstNode, scopes: &ScopesStack) {
-    let mut values: Vec<&String> = vec![];
+    let mut values: Vec<String> = vec![];
 
     for child in node.children {
-        if child.token_type == AstTokenType::Identifier {
-            if let Some(value) = scopes.get_identifier_value(&child.value) {
-                values.push(value);
-            } else {
-                println!("[cei] '{}' is not defined", child.value);
-                std::process::exit(1);
+        match child.token_type {
+            AstTokenType::Identifier => {
+                if let Some(value) = scopes.get_identifier_value(&child.value) {
+                    values.push(value.clone());
+                } else {
+                    println!("[cei] '{}' is not defined", child.value);
+                    std::process::exit(1);
+                }
             }
+            AstTokenType::StringLiteral => {
+                let a = child.value.clone();
+                values.push(a);
+            }
+            _ => {}
         }
     }
 
-    println!("{:?}", values);
-    /*     let string_node = &node.children[0];
-    let mut string_chars = string_node.value.chars();
+    let formatted_values: Vec<String> = values
+        .iter()
+        .map(|val| {
+            let mut val = val.chars();
+            val.next();
+            val.next_back();
+            val.as_str().to_string()
+        })
+        .collect();
 
-    // remove first and last quote
-    string_chars.next();
-    string_chars.next_back();
-    let string_literal = string_chars.as_str();
-
-    println!("{}", string_literal) */
+    println!("{}", formatted_values.join(""));
 }
