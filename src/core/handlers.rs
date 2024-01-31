e964be8c70253581d76d037feda3e03c0e575f33
noreplydev <contact.cristiansanchez@gmail.com>
2024-01-31 20:37:11 +0100

    [FIX] Print handler not working caused by group node

--- src/core/handlers.rs
+++ src/core/handlers.rs
@@ -6,25 +6,31 @@ use crate::{
 pub fn print(node: AstNode, scopes: &ScopesStack) {
     let mut values: Vec<String> = vec![];
 
-    for child in node.children {
-        match child.node_type {
-            AstNodeType::Expression(exp) => match exp {
-                Expression::Identifier => {
-                    if let Some(value) = scopes.get_identifier_value(&child.value.to_string()) {
-                        values.push(value.clone());
-                    }
+    for (i, child) in node.children.iter().enumerate() {
+        if child.node_type == AstNodeType::Group && i == 0 {
+            for child in &child.children {
+                match child.node_type {
+                    AstNodeType::Expression(exp) => match exp {
+                        Expression::Identifier => {
+                            if let Some(value) =
+                                scopes.get_identifier_value(&child.value.to_string())
+                            {
+                                values.push(value.clone());
+                            }
+                        }
+                        Expression::StringLiteral => {
+                            values.push(child.value.to_string());
+                        }
+                        Expression::NumberLiteral => {
+                            values.push(child.value.to_string());
+                        }
+                        Expression::BinaryOperator => {
+                            values.push(child.value.to_string());
+                        }
+                    },
+                    _ => {}
                 }
-                Expression::StringLiteral => {
-                    values.push(child.value.to_string());
-                }
-                Expression::NumberLiteral => {
-                    values.push(child.value.to_string());
-                }
-                Expression::BinaryOperator => {
-                    values.push(child.value.to_string());
-                }
-            },
-            _ => {}
+            }
         }
     }
 
