e68a11fc6fdcd3af70d99c44f1faed1c816e027f
noreplydev <contact.cristiansanchez@gmail.com>
2024-04-09 00:59:32 +0200

    Merge expressions parsing functions to the Module class

--- src/syntax/parser/mod.rs
+++ src/syntax/parser/mod.rs
@@ -1,5 +1,3 @@
-mod expressions;
-
 use crate::{
     core::error::{self, ErrorType},
     syntax::{
@@ -17,7 +15,7 @@ use crate::{
     },
 };
 
-use self::expressions::{expression, parse_expression};
+use super::binary_expression::BinaryExpression;
 
 pub struct Module {
     module_name: String,
@@ -67,7 +65,7 @@ impl Module {
                     current += index_offset;
                 }
                 LexerTokenType::Number => {
-                    let (index_offset, number_node) = expression(&tokens, current);
+                    let (index_offset, number_node) = Self::expression(&tokens, current);
                     module_ast.add_child(number_node);
                     current += index_offset;
                 }
@@ -145,7 +143,7 @@ impl Module {
                     offset += index_offset;
                 }
                 LexerTokenType::Number => {
-                    let (index_offset, number_node) = expression(&tokens, current);
+                    let (index_offset, number_node) = Self::expression(&tokens, current);
                     block_node.add_child(number_node);
                     current += index_offset;
                     offset += index_offset;
@@ -238,7 +236,7 @@ impl Module {
                     break;
                 }
                 _ => {
-                    let (node_offset, node) = parse_expression(tokens, current);
+                    let (node_offset, node) = Self::parse_expression(tokens, current);
                     match node {
                         Expression::Identifier(_) => last_token = Some(LexerTokenType::Identifier),
                         Expression::Bool(_) => last_token = Some(LexerTokenType::TrueKeyword),
@@ -386,7 +384,7 @@ impl Module {
         offset += 1;
 
         // get variable value
-        let (expr_offset, expr) = parse_expression(tokens, current);
+        let (expr_offset, expr) = Self::parse_expression(tokens, current);
         current += expr_offset;
         offset += expr_offset;
 
@@ -547,5 +545,205 @@ impl Module {
         offset += node_offset;
 
         (offset, node)
+    }
+
+    // (2 * 2) + 3
+    fn expression(tokens: &Vec<LexerToken>, current: usize) -> (usize, AstNodeType) {
+        let (offset, expr) = Self::parse_expression(tokens, current);
+        (offset, AstNodeType::Expression(expr))
+    }
+
+    // 2 + 3 * 23
+    fn parse_expression(tokens: &Vec<LexerToken>, current: usize) -> (usize, Expression) {
+        // will autoincrement current
+        // and it will be the root or the left node
+        // depending on the expression
+        let (node_offset, mut node) = Self::parse_term(tokens, current);
+        let mut offset = 0 + node_offset;
+        let mut current = current + offset;
+
+        while current < tokens.len() {
+            match tokens[current].token_type {
+                LexerTokenType::AddOperator | LexerTokenType::SubtractOperator => {
+                    let curr_token = &tokens[current];
+
+                    // consume the operator
+                    let operator = if let Some(op) = curr_token.value.chars().next() {
+                        op
+                    } else {
+                        error::throw(
+                            ErrorType::ParsingError,
+                            format!("Operator '{}' cannot be parsed as char", curr_token.value)
+                                .as_str(),
+                            Some(curr_token.line),
+                        );
+                        std::process::exit(1);
+                    };
+                    current += 1;
+                    offset += 1;
+
+                    // get right node
+                    let (right_offset, right) = Self::parse_term(tokens, current);
+                    current += right_offset;
+                    offset += right_offset;
+
+                    node = Expression::BinaryExpression(BinaryExpression::new(
+                        operator,
+                        Box::new(node),
+                        Box::new(right),
+                        curr_token.at,
+                        curr_token.line,
+                    ));
+                }
+                _ => break,
+            }
+        }
+
+        (offset, node)
+    }
+
+    fn parse_term(tokens: &Vec<LexerToken>, current: usize) -> (usize, Expression) {
+        // will autoincrement current
+        // and it will be the root or the left node
+        // depending on the expression
+        let (node_offset, mut node) = Self::parse_factor(tokens, current);
+        let mut offset = 0 + node_offset;
+        let mut current = current + offset;
+
+        while current < tokens.len() {
+            match tokens[current].token_type {
+                LexerTokenType::MultiplyOperator | LexerTokenType::DivideOperator => {
+                    let curr_token = &tokens[current];
+
+                    // consume the operator
+                    let operator = if let Some(op) = curr_token.value.chars().next() {
+                        op
+                    } else {
+                        error::throw(
+                            ErrorType::ParsingError,
+                            format!("Operator '{}' cannot be parsed as char", curr_token.value)
+                                .as_str(),
+                            Some(curr_token.line),
+                        );
+                        std::process::exit(1);
+                    };
+                    current += 1;
+                    offset += 1;
+
+                    // get right node
+                    let (right_offset, right) = Self::parse_factor(tokens, current);
+                    current += right_offset;
+                    offset += right_offset;
+
+                    node = Expression::BinaryExpression(BinaryExpression::new(
+                        operator,
+                        Box::new(node),
+                        Box::new(right),
+                        curr_token.at,
+                        curr_token.line,
+                    ));
+                }
+                _ => break,
+            }
+        }
+
+        (offset, node)
+    }
+
+    fn parse_factor(tokens: &Vec<LexerToken>, current: usize) -> (usize, Expression) {
+        match tokens[current].token_type {
+            LexerTokenType::OpenParenthesis => {
+                let mut offset = 1; // to consume open parenthesis
+                let mut current = current + offset;
+
+                let (expr_offset, expr) = Self::parse_expression(tokens, current);
+                current += expr_offset;
+                offset += expr_offset;
+
+                if tokens[current].token_type == LexerTokenType::CloseParenthesis {
+                    offset += 1; // to consume close parenthesis
+                    (offset, expr)
+                } else {
+                    error::throw(
+                        ErrorType::ParsingError,
+                        format!("Unexpected token '{}', expected ')'", tokens[current].value)
+                            .as_str(),
+                        Some(tokens[current].line),
+                    );
+                    std::process::exit(1);
+                }
+            }
+            LexerTokenType::Number => {
+                let number_node = Number::from_string(
+                    tokens[current].value.clone(),
+                    tokens[current].at,
+                    tokens[current].line,
+                );
+
+                if let Some(node) = number_node {
+                    // 1 since it only consumes the number token.
+                    // in the future this will be inside a struct and
+                    // will mutate internal state
+                    (1, Expression::Number(node))
+                } else {
+                    error::throw(
+                        ErrorType::ParsingError,
+                        format!(
+                            "Invalid token '{}' inside of a expression",
+                            tokens[current].value
+                        )
+                        .as_str(),
+                        Some(tokens[current].line),
+                    );
+                    std::process::exit(1);
+                }
+            }
+            LexerTokenType::TrueKeyword | LexerTokenType::FalseKeyword => {
+                let node = if let Ok(bool_value) = tokens[current].value.parse::<bool>() {
+                    Bool::new(bool_value, tokens[current].at, tokens[current].line)
+                } else {
+                    error::throw(
+                        ErrorType::ParsingError,
+                        format!(
+                            "Invalid token '{}' inside of a expression",
+                            tokens[current].value
+                        )
+                        .as_str(),
+                        Some(tokens[current].line),
+                    );
+                    std::process::exit(1);
+                };
+
+                (1, Expression::Bool(node))
+            }
+            LexerTokenType::StringLiteral => (
+                1,
+                Expression::StringLiteral(StringLiteral::new(
+                    tokens[current].value.clone(),
+                    tokens[current].at,
+                    tokens[current].line,
+                )),
+            ),
+            LexerTokenType::Identifier => (
+                1,
+                Expression::Identifier(Identifier::new(
+                    tokens[current].value.clone(),
+                    tokens[current].at,
+                    tokens[current].line,
+                )),
+            ),
+            _ => {
+                error::throw(
+                    error::ErrorType::SyntaxError,
+                    format!(
+                        "Invalid token '{}' inside of a expression",
+                        tokens[current].value
+                    )
+                    .as_str(),
+                    Some(tokens[current].line),
+                );
+                std::process::exit(1);
+            }
+        }
     }
 }
