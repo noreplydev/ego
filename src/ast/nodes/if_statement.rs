7494de0b90fafea2a90f305a045ba136a36b9578
noreplydev <contact.cristiansanchez@gmail.com>
2024-04-18 23:46:53 +0200

    fix if statement class

--- src/ast/nodes/if_statement.rs
+++ src/ast/nodes/if_statement.rs
@@ -1,15 +1,15 @@
-use super::{block::Block, group::Group};
+use super::{block::Block, Expression};
 
 #[derive(Debug, Clone)]
 pub struct IfStatement {
-    pub condition: Group,
+    pub condition: Expression,
     pub body: Block,
     pub at: usize,
     pub line: usize,
 }
 
 impl IfStatement {
-    pub fn new(condition: Group, body: Block, at: usize, line: usize) -> IfStatement {
+    pub fn new(condition: Expression, body: Block, at: usize, line: usize) -> IfStatement {
         IfStatement {
             condition,
             body,
