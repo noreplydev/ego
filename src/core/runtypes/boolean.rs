029263f787334394d2c89922343a7c9ee289dfcd
noreplydev <contact.cristiansanchez@gmail.com>
2024-05-05 02:47:07 +0200

    Add new comparison operators to the boolean type

--- src/core/runtypes/boolean.rs
+++ src/core/runtypes/boolean.rs
@@ -86,4 +86,38 @@ impl RnBoolean {
             RuntimeType::RnFunction(_) => RuntimeType::boolean(false),
         }
     }
+    pub fn greater_than_or_equal(
+        &self,
+        operand: RuntimeType,
+        _scopes: &ScopesStack,
+    ) -> RuntimeType {
+        match operand {
+            RuntimeType::Nothing(_) => RuntimeType::boolean(false),
+            RuntimeType::RnString(_) => RuntimeType::boolean(false),
+            RuntimeType::RnBoolean(v) => RuntimeType::boolean(self.val == v.val),
+            RuntimeType::RnNumber(_) => RuntimeType::boolean(false),
+            RuntimeType::RnIdentifier(_) => RuntimeType::boolean(false),
+            RuntimeType::RnFunction(_) => RuntimeType::boolean(false),
+        }
+    }
+    pub fn not_equal(&self, operand: RuntimeType, _scopes: &ScopesStack) -> RuntimeType {
+        match operand {
+            RuntimeType::Nothing(_) => RuntimeType::boolean(true),
+            RuntimeType::RnString(_) => RuntimeType::boolean(true),
+            RuntimeType::RnBoolean(v) => RuntimeType::boolean(self.val != v.val),
+            RuntimeType::RnNumber(_) => RuntimeType::boolean(false),
+            RuntimeType::RnIdentifier(_) => RuntimeType::boolean(true),
+            RuntimeType::RnFunction(_) => RuntimeType::boolean(true),
+        }
+    }
+    pub fn equal(&self, operand: RuntimeType, _scopes: &ScopesStack) -> RuntimeType {
+        match operand {
+            RuntimeType::Nothing(_) => RuntimeType::boolean(false),
+            RuntimeType::RnString(_) => RuntimeType::boolean(false),
+            RuntimeType::RnBoolean(v) => RuntimeType::boolean(self.val == v.val),
+            RuntimeType::RnNumber(_) => RuntimeType::boolean(false),
+            RuntimeType::RnIdentifier(_) => RuntimeType::boolean(false),
+            RuntimeType::RnFunction(_) => RuntimeType::boolean(false),
+        }
+    }
 }
