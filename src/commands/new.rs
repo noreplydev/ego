5685f13d00e900d7e9cc5cfc48e10f67929a03e4
noreplydev <contact.cristiansanchez@gmail.com>
2024-05-17 02:10:40 +0200

    Refactor new command

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -20,14 +20,40 @@ impl New {
             Ok(_) => {
                 // Create main file
                 path.push("main.ego");
-                if let Ok(mut file) = File::create(path) {
+                if let Ok(mut file) = File::create(&path) {
                     match File::write(&mut file, "print(\"Hello, Ego!\")".as_bytes()) {
                         Ok(_) => println!(" ⚈ Succesfully initialized: {}\n", self.args[0].clone()),
-                        Err(_) => println!(" Failed to create new ego project"),
+                        Err(_) => println!(" ⅹ Failed to create new ego project"),
                     }
                 }
+
+                // Create config file
+                path.pop();
+                path.push("ego.yaml");
+
+                match File::create(&path) {
+                    Ok(mut file) => {
+                        if let Some(parent) = path.parent() {
+                            if let Some(dir_name) = parent.file_name() {
+                                let config_data = format!(
+                                    "[{}]\nego_version: 0.0.1\npackage_name: {}\nversion: 1.0.0",
+                                    dir_name.to_string_lossy(),
+                                    dir_name.to_string_lossy()
+                                );
+                                if let Err(e) = file.write_all(config_data.as_bytes()) {
+                                    println!("Failed to write to ego.yaml: {}", e);
+                                }
+                            } else {
+                                println!(" ⅹ Failed to retrieve directory name for config file");
+                            }
+                        } else {
+                            println!(" ⅹ Failed to retrieve parent directory for config file");
+                        }
+                    }
+                    Err(_) => println!(" ⅹ Failed to create ego.yaml"),
+                }
             }
-            Err(_) => println!(" Failed to create new ego project"),
+            Err(_) => println!(" ⅹ Failed to create new ego project"),
         }
     }
 }
