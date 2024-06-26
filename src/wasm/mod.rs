a95b87e6380bb888b0ed6939f1704007b4d94a68
noreplydev <contact.cristiansanchez@gmail.com>
2024-06-27 01:14:54 +0200

    Add some logging info for wasm runtime

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -1,13 +1,23 @@
 use crate::{
     ast::{lex, Module},
     compiler::Compiler,
+    log,
     runtime::Interpreter,
 };
 
 pub fn run_ego(code: String, vm: bool) {
+    log!("Executing ego: \n  - code: {}\n  - vm: {}", code, vm);
     let tokens = lex(code);
+    log!(" > Generated tokens");
+
     let mut module = Module::new("fileless".to_string(), tokens);
     let ast = module.parse();
+    log!(" > Generated ast");
+    log!(
+        " > Running on {}",
+        if vm { "self vm" } else { "Ast interpreter" }
+    );
+
     if vm {
         let bytecode = Compiler::gen_bytecode(ast);
         let mut vm = self_vm::vm::Vm::new(bytecode);
